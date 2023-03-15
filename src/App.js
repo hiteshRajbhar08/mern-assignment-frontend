fa91a29ee08c02d7f25cdd251a5a49fa6b1b08ba
Hitesh <h08rajbhar@gmail.com>
2023-03-15 14:02:52 +0530

    creating home page and adding react router for switching pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
-        <Container></Container>
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,27 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="#home">Ecommerce</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>Ecommerce</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home">
-                <i className="fas fa-shopping-cart"></i> Cart
-              </Nav.Link>
-              <Nav.Link href="#link">
-                <i className="fas fa-user"></i> SignIn
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/signin">
+                <Nav.Link>
+                  <i className="fas fa-user"></i> SignIn
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
